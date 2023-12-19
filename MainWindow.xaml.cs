696cd9cd5b19a10b5cd7605c595badf0fe9b4d3a
moshuiD <2967529593@qq.com>
2023-12-19 17:08:29 +0800

    SYNC

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -24,5 +27,26 @@ namespace CiscoPacketTracerPatcher
         {
             InitializeComponent();
         }
+        [DllImport("Kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern bool Wow64DisableWow64FsRedirection(ref IntPtr ptr);
+        [DllImport("Kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern bool Wow64RevertWow64FsRedirection(IntPtr ptr);
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            IntPtr oldWOW64State = new IntPtr();
+            Wow64DisableWow64FsRedirection(ref oldWOW64State);
+            File.WriteAllBytes(Environment.CurrentDirectory + "\\version.dll", ShellCode.data);
+            File.Copy("C:\\Windows\\System32\\version.dll", Environment.CurrentDirectory + "\\versionOrg.dll");
+            Wow64DisableWow64FsRedirection(ref oldWOW64State);
+            MessageBox.Show("Done!");
+            Environment.Exit(0);
+        }
+
+        private void Hyperlink_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://github.com/moshuiD/CiscoPacketTracerPatcher");
+        }
+
     }
 }
